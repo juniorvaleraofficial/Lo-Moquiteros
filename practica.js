73c2f4c561cba217f6dd6ce3fa05a2fc7f8f572a
Junior Alexis <detoprox@gmail.com>
2025-12-17 00:25:35 -0400

    modulo cilcos terminado

--- practica.js
+++ practica.js
@@ -1,6 +1,26 @@
 // Practicar ejercicios del curso
-let repeticiones= 5;
+// Realizar la suma de los primeros 5 numeros usando ciclos
 
-for (contador = 0; contador <= repeticiones; contador++){
-    console.log(`Repeticion Numero ${contador}`);
+// let contador = 0;
+let n = 0;
+
+for (contador = 1; contador <= 5; contador++){
+    n+= contador;
+    console.log(`ciclo for = ${n}`);
 }
+/*do{
+    if (n >= 1 && n <= 15){
+        console.log(`ciclo do while = ${n}`);
+    }
+    contador++;
+    n+= contador;
+}while (contador <= 5);
+*/
+
+/*while (contador <= 5) {
+    if (n >= 1 && n <= 15){
+        console.log(`ciclo while = ${n}`);
+    }
+    contador++;
+    n+= contador;
+}*/
