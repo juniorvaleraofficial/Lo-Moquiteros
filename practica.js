f8db4a348ba782d6266b0168cdf5ff89fb97e6f8
Junior Alexis <detoprox@gmail.com>
2025-12-17 10:39:22 -0400

    Ejemplo de arreglos en JavaScript

--- practica.js
+++ practica.js
@@ -1,10 +1,14 @@
 // Modulo Arreglos en JavaScript
+// Ejemplo de arreglos en JavaScript
 
 let numerosArreglos = [];
 
 numerosArreglos[0] = 30;
-numerosArreglos[4] = 25;
+numerosArreglos[1] = 25;
+numerosArreglos[4] = "HOLA";
 
-console.log(numerosArreglos[0]);
-console.log(numerosArreglos[5]);
-console.log(numerosArreglos[4]);
+console.log(`Elemento 1 ${numerosArreglos[0]}`);
+console.log(`Elemento 2 ${numerosArreglos[1]}`);
+console.log(`Elemento 3 ${numerosArreglos[2]}`);
+console.log(`Elemento 4 ${numerosArreglos[3]}`);
+console.log(`Elemento 5 ${numerosArreglos[4]}`);
