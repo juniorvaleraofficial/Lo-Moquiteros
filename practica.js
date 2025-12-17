b9345241a0c05d250c3387e6a6ddd5a27aa860ee
Junior Alexis <detoprox@gmail.com>
2025-12-17 00:45:28 -0400

    Arreglos en JavaScript

--- practica.js
+++ practica.js
@@ -1,1 +1,10 @@
 // Modulo Arreglos en JavaScript
+
+let numerosArreglos = [];
+
+numerosArreglos[0] = 30;
+numerosArreglos[4] = 25;
+
+console.log(numerosArreglos[0]);
+console.log(numerosArreglos[1]);
+console.log(numerosArreglos[4]);
