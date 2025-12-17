ff27c355a3762726aa0adb68fcfee7d035fce7b5
Junior Alexis <detoprox@gmail.com>
2025-12-17 11:16:03 -0400

    Sintaxis simplificada de arreglos en JavaScript

--- practica.js
+++ practica.js
@@ -1,11 +1,8 @@
 // Modulo Arreglos en JavaScript
-// Ejemplo de arreglos en JavaScript
+// Sintaxis simplificada de arreglos en JavaScript
 
-let numerosArreglos = [];
+let numerosArreglos = [25,35,5,1,3];
 
-numerosArreglos[0] = 30;
-numerosArreglos[1] = 25;
-numerosArreglos[4] = "HOLA";
 
 console.log(`Elemento 1 ${numerosArreglos[0]}`);
 console.log(`Elemento 2 ${numerosArreglos[1]}`);
