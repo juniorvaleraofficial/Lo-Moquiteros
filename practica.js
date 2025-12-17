6457a43006d4089fa81c1f4592626d4dafcfea2c
Junior Alexis <detoprox@gmail.com>
2025-12-17 11:30:41 -0400

    Iterar arreglos en JavaScript

--- practica.js
+++ practica.js
@@ -1,11 +1,8 @@
 // Modulo Arreglos en JavaScript
-// Sintaxis simplificada de arreglos en JavaScript
+// Iterar arreglos en JavaScript
 
 let numerosArreglos = [25,35,5,1,3];
 
-
-console.log(`Elemento 1 ${numerosArreglos[0]}`);
-console.log(`Elemento 2 ${numerosArreglos[1]}`);
-console.log(`Elemento 3 ${numerosArreglos[2]}`);
-console.log(`Elemento 4 ${numerosArreglos[3]}`);
-console.log(`Elemento 5 ${numerosArreglos[4]}`);
+for(let i = 0; i < numerosArreglos.length; i++){
+    console.log(`indice numerosArreglos[${i}] = ${numerosArreglos[i]}`);
+}
