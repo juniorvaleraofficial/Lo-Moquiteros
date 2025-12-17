a8a8bfc2200daa75c616a34b950f17486547e00e
Junior Alexis <detoprox@gmail.com>
2025-12-17 10:34:44 -0400

    Empezar operaciones basicas de arreglos

--- practica.js
+++ practica.js
@@ -6,5 +6,5 @@ numerosArreglos[0] = 30;
 numerosArreglos[4] = 25;
 
 console.log(numerosArreglos[0]);
-console.log(numerosArreglos[1]);
+console.log(numerosArreglos[5]);
 console.log(numerosArreglos[4]);
