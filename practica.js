86b5f9cdbe9cc7a12d6162cc47ca9cf4b1e3bec2
Junior Alexis <detoprox@gmail.com>
2025-12-19 12:32:51 -0400

    con cadenas no cambia el valor en paso de cadenas

--- practica.js
+++ practica.js
@@ -1,12 +1,12 @@
 // Modulo Funciones en JavaScript
-// Objetos (Arrays) se pasan por referencia
+// Ejemplo de paso de cadena en JS
 
 function cambiarValor(parametro){
-    parametro[0] = 20;
+    parametro = "adios"
 }
 
 // Llama a la funcion
-let arreglo = [10];
-console.log(`Valor Antes de la funcion ${arreglo[0]}`);// paso por valor 
-cambiarValor(arreglo);
-console.log(`Valor despues de la funcion: ${arreglo[0]}`);// paso por referencia
+let cadena = "hola"
+console.log(`Valor Antes de la funcion ${cadena}`);
+cambiarValor(cadena);
+console.log(`Valor despues de la funcion: ${cadena}`);
