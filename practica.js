c39359a1d1670f92484800c8640874d94776469b
Junior Alexis <detoprox@gmail.com>
2025-12-18 22:57:03 -0400

    Iterar una matriz en JavaScript

--- practica.js
+++ practica.js
@@ -1,28 +1,19 @@
 // Modulo Matrices en JavaScript
-// Sintaxis simplificada de matrices en JavaScript
+// Iterar una matriz en JavaScript
 
-let matriz=[[],[],[]];
-let matrizSimple = [[10,20,30],[100,200,300],[1000,2000,3000]];// sintaxis simplificada
+let matriz = [[10,20,30],[100,200,300],[1000,2000,3000]];// sintaxis simplificada
 
-// Modificar los valores 
-// Renglon [0]
-matriz[0][0] = 50;
-matriz[0][1] = 100;
-matriz[0][2] = 75;
+console.log(matriz.length);
+console.log(matriz[0].length);
 
-// Renglon [1]
-matriz[1][0] = 150;
-matriz[1][1] = 200;
-matriz[1][2] = 500;
+for (let fila = 0; fila < matriz.length; fila++) {
+    for (let col = 0; col < matriz[fila].length; col++){
+        console.log(`matriz[${fila}][${col}] = ${matriz[fila][col]}`);
+    }
+}
 
-// Renglon [2]
-matriz[2][0] = 250;
-matriz[2][1] = 300;
-matriz[2][2] = 350;
-
-
-// Leer los valores
-console.log("Matriz normal: ");
-console.log(`matriz[2][2] = ${matriz[2][2]}`);
-console.log("matriz simplificada: ");
-console.log(`matrizSimple[0][1]; = ${matrizSimple[0][2]}`);
+for (let i = 0; i < matriz.length; i++) {
+    const element = matriz[i];
+    console.log(element);
+    
+}
