7fcd103f72a40f223b9a580d8eef75cf5df3d3d5
Junior Alexis <detoprox@gmail.com>
2025-12-21 23:12:15 -0400

    Funciones incorporadas

--- practica.js
+++ practica.js
@@ -1,14 +1,21 @@
-// Modulo Funciones en JavaScript
-// Funciones Recursivas en JS
+// Modulo Funciones incorporadas JavaScript
 
-function funcionRecursiva(numero){
-    if(numero == 1){
-        console.log(numero);
-    }
-    else{
-        console.log(numero);
-        funcionRecursiva(numero -1);
-    }
+// obtener el largo de una cadena
+
+let cadena1 = 'que tal?..';
+
+console.log(cadena1.length);
+
+// no podemos modificar una cadena en JavaScript 
+// las cadenas son inmutables
+cadena1[0] = 'x';
+console.log(cadena1);
+
+// si podemos asignar una nueva cadena 
+cadena1 = 'hola...';
+console.log(cadena1);
+
+// recorrer los caracteres de una cadena 
+for(let i= 0; i < cadena1.length; i++){
+    console.log(` indice ={${i}} valor = {${cadena1[i]}}`);
 }
-// Llama a la funcion
-funcionRecursiva(20);
