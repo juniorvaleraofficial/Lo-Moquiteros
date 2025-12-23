652359487eebea9f0da6e0e936a5a77f215ccf7d
Junior Alexis <detoprox@gmail.com>
2025-12-22 23:55:49 -0400

    objetos en JavaScript

---
+++ Curso_Javascript/objetos/persona.js
@@ -0,0 +1,14 @@
+// diferencia entre objetos y tipo primitivos
+
+// primitivo
+let x = 10;
+console.log(typeof(x));
+// objeto
+let persona = {
+    nombre: 'Junior',
+    apellido: 'Valera',
+    edad: 30,
+    email: 'detoprox@gmail.com'
+}
+
+console.log(typeof(persona));
