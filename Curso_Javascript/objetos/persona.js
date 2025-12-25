e7e7a7594cc890afb95a07de3a17177268730c8b
Junior Alexis <detoprox@gmail.com>
2025-12-24 21:44:39 -0400

    metodo GET en Objetos

--- Curso_Javascript/objetos/persona.js
+++ Curso_Javascript/objetos/persona.js
@@ -1,25 +1,13 @@
-// diferencia entre objetos y tipo primitivos
+// Objetos 
 // funciones en objetos
 let persona = {
     nombre: 'Junior',
     apellido: 'Valera',
     edad: 30,
     email: 'detoprox@gmail.com',
-    nombreCompleto: function(){
+   get nombreCompleto(){
         return this.nombre + ' ' + this.apellido;
     }
 }
 
-/** for (const nombrePropiedad in persona) {
-    
-    console.log(persona[nombrePropiedad]);
-}
-
-
-let personaArray = Object.values(persona);
-console.log(personaArray);
-
-*/
-
-let personaString = JSON.stringify(persona);
-console.log(personaString);
+console.log(persona.nombreCompleto);
