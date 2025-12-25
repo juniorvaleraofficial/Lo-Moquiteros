48c3e972a9d03b4eac2304557abc45d40201dda0
Junior Alexis <detoprox@gmail.com>
2025-12-25 19:34:00 -0400

    Uso de metodo call para usar atributos de un metodo en otro

--- Curso_Javascript/objetos/persona.js
+++ Curso_Javascript/objetos/persona.js
@@ -1,18 +1,17 @@
-// Funcion constructor de tipo persona
-// uso de Prototype en JS
-
-function Persona(nombre, apellido, email){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.nombreCompleto = function(){
-        return this.nombre +' '+ this.apellido;
+let persona = {
+    nombre: 'Pedro',
+    apellido: 'Rondon',
+    nombreCompleto: function(){
+        return this.nombre + ' ' + this.apellido;
     }
 }
-Persona.prototype.tel = '809-966-3715';
 
-let padre = new Persona('Martin', 'Peralta', 'mPeralta@mail.com');
-let madre = new Persona('Raquel', 'Rivera', 'rRivera@mail.com');
-console.log(padre.tel);
-madre.tel = '829-440-4818';
-console.log(madre.tel);
+let persona2 = {
+    nombre: 'Mario',
+    apellido: 'Fulcal'
+}
+
+// uso del metodo call
+// para usar el metodo en persona.nombreCompleto con los datos de persona2
+
+console.log(persona.nombreCompleto.call(persona2));
