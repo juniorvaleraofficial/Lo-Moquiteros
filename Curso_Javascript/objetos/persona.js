8c58b084034e59fd36028a9bf64ea59e7f680582
Junior Alexis <detoprox@gmail.com>
2025-12-26 23:07:49 -0400

    uso de GET y SET en JS

--- Curso_Javascript/objetos/persona.js
+++ Curso_Javascript/objetos/persona.js
@@ -1,11 +1,20 @@
 // uso de clases en JS
+// uso de GET y SET en JS
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        return this._nombre = nombre;
     }
 }
 
 let persona1 = new Persona('Junior', 'Valera');
-
+persona1._nombre = 'Junior Alexis';
 console.log(persona1);
