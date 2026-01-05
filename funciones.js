98e75ac0f7d655571436b0d4dda16519dc853be3
ENRIQUEZJO <166477080+ENRIQUEZJO@users.noreply.github.com>
2026-01-05 02:01:02 -0400

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,16 @@
+//PRIMERA FUNCION QUE YO REALICE
+
+function sumar(a, b){
+    let resultado = a + b;
+    return resultado;
+}
+
+let a=2, b=8, c;
+
+c = sumar (a, b);
+
+console.log(`Resultado sumar:`,c);
+
+
+// mañana sigo
+
