ae5a7d18384c8d06cceaabb2734be57053f9c146
Junior Alexis <detoprox@gmail.com>
2026-03-22 00:38:33 -0400

    uso de setTimeout para funciones asincornas

--- funciones Callback/fcallback.js
+++ funciones Callback/fcallback.js
@@ -10,3 +10,19 @@ function sumar(n1, n2, msj){
 }
 
 sumar(5,5, mensaje);
+
+// funciones asincronas con setTimeOut
+
+function mifuncionCallback(){
+    console.log('Se ejecuta despues de 3 segundos: ');
+}
+
+setTimeout(mifuncionCallback,3000);
+
+setTimeout(() => console.log(`se ejecuta despues de 1 segundos`),1000);
+
+setTimeout(() => console.log(`se ejecuta despues de 2 segundos`),2000);
+
+setTimeout(() => console.log(`se ejecuta despues de 4 segundos`),4000);
+
+setTimeout(function (){console.log('Se ejecuta despues de 5 segundos');}, 5000 );
